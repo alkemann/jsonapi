a07c6d7a9017f426b693fa0627c2e37eed1c2454
Alexander Morland <alek.morland@gmail.com>
2017-09-08 17:02:27 +0200

    Completed Model test

--- tests/unit/ModelTest.php
+++ tests/unit/ModelTest.php
@@ -1,34 +1,40 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: alek
- * Date: 08/09/2017
- * Time: 16:24
- */
 
 namespace alkemann\jsonapi\tests\unit;
 
+use alkemann\jsonapi\exceptions\InternalSeverError;
+use alkemann\jsonapi\Model;
 use alkemann\jsonapi\tests\mocks\Posts;
+
 
 class ModelTest extends \PHPUnit_Framework_TestCase
 {
-
     public function testSerialize()
     {
         $data = ['id' => 12, 'title' => 'Winning', 'status' => 'ACTIVE'];
         $m = new Posts($data);
 
         $expected = [
-            'data' => [
-                'type' => 'posts',
-                'id' => 12,
-                'attributes' => [
-                    'title' => 'Winning',
-                    'status' => 'ACTIVE'
-                ]
+            'type' => 'posts',
+            'id' => 12,
+            'attributes' => [
+                'title' => 'Winning',
+                'status' => 'ACTIVE'
             ]
         ];
         $result = $m->jsonSerialize();
         $this->assertEquals($expected, $result);
+    }
+
+    public function testSave()
+    {
+        $this->expectException(InternalSeverError::class);
+        $data = ['id' => 12, 'title' => 'Winning', 'status' => 'ACTIVE'];
+        $m = new class($data) extends Model {
+            public function saveModel(array $data = [], array $options = []): bool {
+                return false;
+            }
+        };
+        $m->save($data);
     }
 }
